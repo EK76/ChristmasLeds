2fba0403298e7678a6b44364da6518ef74191ad2
Ken Ekholm <ken.ekholm@live.com>
2026-05-20 15:04:45 +0300

    Added new files.

--- ChristmasLeds.ino
+++ ChristmasLeds.ino
@@ -1,23 +1,24 @@
 #include <SoftwareSerial.h>
-int redLeds[] = {11, 5, 6, 9, 10}; 
-volatile int period=5000;
-int period2=20;
-int period3=1500;
+int redLeds[] = {5, 6, 9, 10, 11}; 
+int interval=2000;
+int interval2=20;
 volatile int counter = 0;
 volatile int counter2 = 0;
 volatile int changeValue = 1;
 int ledCounter=1;
+unsigned long previousDelay = 0; 
 long randomNumber;
 bool checkOnce = false;
-unsigned long time_now = 0;
-unsigned long time_now2 = 0;
-unsigned long time_now3 = 0;
+unsigned long currentDelay;
+unsigned long lastDebouncetime = 0;
+const unsigned long debounceDelay = 250;
+unsigned long timeNow= 0;
+unsigned long timeNow2 = 0;
 
 void setup() {
   Serial.begin(115200);
   randomSeed(analogRead(0));
   delay(500);
-  pinMode(4, OUTPUT);
   pinMode(5, OUTPUT);
   pinMode(6, OUTPUT);
   pinMode(8, OUTPUT);
@@ -28,48 +29,37 @@ void setup() {
   pinMode(2, INPUT_PULLUP);
   pinMode(3, INPUT_PULLUP);
   attachInterrupt(digitalPinToInterrupt(2), choiceValue, CHANGE);
-  attachInterrupt(digitalPinToInterrupt(3), delayValue, CHANGE);
-  digitalWrite(4, LOW);
-  digitalWrite(8, HIGH);
-  digitalWrite(12, LOW);
+  digitalWrite(8, LOW);
+  digitalWrite(12, HIGH);
 }
 
 void delayTime()
 { 
-   time_now = millis();
-   while(millis() - time_now < period){}
-}
-
-void delayTime2()
-{ 
-   time_now2 = millis();
-   while(millis() - time_now2 < period2){} 
-}
-
-void delayTime3()
-{ 
-   time_now3 = millis();
-   while(millis() - time_now3 < period3){} 
+   timeNow = millis();
+   while(millis() - timeNow < interval2){} 
 }
 
 void loop() 
 {
+  currentDelay = millis();
   if (checkOnce == true)
   {
     checkOnce = false;
-
     pinMode(5, OUTPUT);
     pinMode(6, OUTPUT);
     pinMode(9, OUTPUT);
     pinMode(10, OUTPUT);
     pinMode(11, OUTPUT);
-
     Serial.println("TRUE");
   }
   switch(changeValue)
   {
     case 1:    
-      randomNumber = random(6);
+      if (currentDelay - previousDelay >= interval) 
+      {
+        previousDelay = currentDelay;  
+        randomNumber = random(6);
+      }  
       switch(randomNumber)
       {
         case 1:
@@ -113,7 +103,6 @@ void loop()
           Serial.println("Random 5");
           break;  
       }
-      delayTime();
       break;
     case 2:
       for (int ledFade = 255; ledFade >= 0; ledFade--)
@@ -123,19 +112,21 @@ void loop()
         analogWrite(redLeds[2], ledFade);
         analogWrite(redLeds[3], ledFade);
         analogWrite(redLeds[4], ledFade);
-        delayTime2();
+        delayTime();
       }
-      delayTime();
-      for (int ledFade = 0; ledFade <= 255; ledFade++) 
-      {
-        analogWrite(redLeds[0], ledFade);
-        analogWrite(redLeds[1], ledFade);
-        analogWrite(redLeds[2], ledFade);
-        analogWrite(redLeds[3], ledFade);
-        analogWrite(redLeds[4], ledFade);
-        delayTime2();
+      if (currentDelay - previousDelay >= interval) 
+      { 
+        previousDelay = currentDelay;  
+        for (int ledFade = 0; ledFade <= 255; ledFade++) 
+        {
+          analogWrite(redLeds[0], ledFade);
+          analogWrite(redLeds[1], ledFade);
+          analogWrite(redLeds[2], ledFade);
+          analogWrite(redLeds[3], ledFade);
+          analogWrite(redLeds[4], ledFade);
+          delayTime();
+        }
       }
-      delayTime();   
       break;
 
     case 3:
@@ -147,81 +138,67 @@ void loop()
           digitalWrite(redLeds[2], LOW);
           digitalWrite(redLeds[3], LOW);
           digitalWrite(redLeds[4], LOW);
-          delayTime3(); 
           break;
         case 2:
           digitalWrite(redLeds[0], HIGH);
-          delayTime(); 
           break;
         case 3:
           digitalWrite(redLeds[1], HIGH);
-          delayTime(); 
           break;
         case 4:
           digitalWrite(redLeds[2], HIGH);
-          delayTime(); 
           break;
         case 5:
           digitalWrite(redLeds[3], HIGH);
-          delayTime(); 
           break;
         case 6:
           digitalWrite(redLeds[4], HIGH);
-          delayTime(); 
           break;  
         case 7:
           ledCounter=0;
-          delayTime(); 
           break;  
       }
-      ledCounter++;
+
+      if (currentDelay - previousDelay >= interval) 
+      { 
+        previousDelay = currentDelay;  
+        ledCounter++;
+      }  
       break;     
   }
-  Serial.println(period);
+  Serial.println(interval);
   Serial.println(changeValue);
 }
 
 void choiceValue()
 {
-  counter++;
-  switch(counter)
+  if (millis() - lastDebouncetime > debounceDelay) 
   {
-    case 1:
-      changeValue=1;
-      digitalWrite(8, HIGH);
-      digitalWrite(12, LOW);
-      break;
+    lastDebouncetime = millis();
+    previousDelay = 0; 
+    counter++;
+    switch(counter)
+    {
+      case 1:
+        changeValue=2;
+        digitalWrite(8, HIGH);
+        digitalWrite(12, LOW);
+        break;
 
-    case 2:
-      changeValue=2;
-      checkOnce = true;   
-      digitalWrite(8, LOW);
-      digitalWrite(12, HIGH);
-      break;
+      case 2:
+        changeValue=3;
+        digitalWrite(8, HIGH);
+        digitalWrite(12, HIGH);
+        checkOnce = true;   
+        break;
     
-    case 3:
-      changeValue=3;
-      counter=0;
-      digitalWrite(8, HIGH);
-      digitalWrite(12, HIGH);
-      break;
-  }  
-}
+      case 3:
+        changeValue=1;
 
- void delayValue()
-{
-  counter2++;
-  switch(counter2)
-  {
-    case 1:
-      period=1500;
-      digitalWrite(4, HIGH);
-      break;
-
-    case 2:
-      period=5000;
-      counter2=0;
-      digitalWrite(4, LOW);
-      break;
+        digitalWrite(8, LOW);
+        digitalWrite(12, HIGH);
+        counter=0;
+        break;
+    }  
   }
 }
